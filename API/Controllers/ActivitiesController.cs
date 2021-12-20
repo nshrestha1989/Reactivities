b10697d96ae69bff4607b41cc950fec3d3157fb5
NIRANJAN SHRESTHA <niranjanshrestha1989@gmail.com>
2021-12-21 00:21:46 +1030

    validating data and handling APi error response

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -13,21 +13,22 @@ namespace API.Controllers
     {
        [HttpGet]
          
-        public async Task<ActionResult<List<Activity>>> GetActivities(){
-            return await Mediator.Send(new List.Query());
+        public async Task<IActionResult> GetActivities(){
+            return HandleResult(await Mediator.Send(new List.Query()));
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<Activity>> GetActivity(Guid id)
+        public async Task<IActionResult> GetActivity(Guid id)
         {
-            return await Mediator.Send(new Details.Query { Id=id});
+            return HandleResult(await Mediator.Send(new Details.Query { Id = id }));
+
         }
 
       [HttpPost]
 
       public async Task<IActionResult> CreateActivity(Activity activity)
         {
-            return Ok(await Mediator.Send(new Create.Command { Activity = activity }));
+            return HandleResult(await Mediator.Send(new Create.Command { Activity = activity }));
         }
 
         [HttpPut("{id}")]
@@ -37,14 +38,14 @@ namespace API.Controllers
             //adding the id to the body pass by client as sometime they will not pass id in the activity body
             activity.Id = id;
 
-            return Ok(await Mediator.Send(new Edit.Command { Activity = activity }));
+            return HandleResult(await Mediator.Send(new Edit.Command { Activity = activity }));
         }
 
         [HttpDelete("{id}")]
 
         public async Task<IActionResult> DeleteActivity(Guid id)
         {
-            return Ok(await Mediator.Send(new Delete.Command { Id = id }));
+            return HandleResult(await Mediator.Send(new Delete.Command { Id = id }));
         }
     }
 }
